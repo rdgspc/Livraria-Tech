dd58af3d303811eb64c040320dfcd945bdb6331d
Rodrigo <rdgcostacontatos@gmail.com>
2024-03-25 22:31:09 -0300

    Commit Inicial

---
+++ EmprestimoLivros/Models/EmprestimosModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmprestimoLivros.Models
+{
+    public class EmprestimosModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Campo obrigatório")]
+        public string Recebedor { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Fornecedor { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string LivroEmprestado { get; set; }
+
+        public DateTime dataUltimaAtualizacao { get; set; } = DateTime.Now;
+
+    }
+}
